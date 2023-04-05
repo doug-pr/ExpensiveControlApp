cbe7c8ad2f082047c083a84658b03ebb0ba93ca6
Douglas Alves <doug-pr@hotmail.com>
2023-04-05 17:01:40 -0300

    Initial

---
+++ ExpensiveControlApp/DTOs/CreateExpensiveDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensiveControlApp.DTOs
+{
+    public class CreateExpensiveDTO
+    {
+        public CreateExpensiveDTO()
+        {
+            Date = DateTime.Now;
+        }
+        
+        [Required(ErrorMessage = "Descrição é Obrigatória!")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Valor é Obrigatório!")]
+        [Range(0.01, 9999999999, ErrorMessage = "Valor Deve Ser Maior Que 0!")]
+        public double Value { get; set; }
+
+        [Required(ErrorMessage = "Data é Obrigatório!")]
+        public DateTime Date { get; set; }
+    }
+}
